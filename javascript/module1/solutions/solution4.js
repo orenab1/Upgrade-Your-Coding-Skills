727672b0a69163bba2243c92a224400e2aa7fbf6
orenab1 <54227972+orenab1@users.noreply.github.com>
2023-12-28 12:49:18 +0200

    Update solution4.js

--- javascript/module1/solutions/solution4.js
+++ javascript/module1/solutions/solution4.js
@@ -2,15 +2,15 @@ function getPlayerExprience(
     numOfLevel1CreaturesKilled,
     numOfLevel2CreaturesKilled
   ) {
-    const tutorialPoints = 1000;
-    const pointsForKillingLevel1Creature = 500;
-    const pointsForKillingLevel2Creature = 1000;
+  const tutorialPoints = 1000;
+  const pointsForKillingLevel1Creature = 500;
+  const pointsForKillingLevel2Creature = 1000;
   
-    const pointsForLevel1CreaturesKilled =
-      numOfLevel1CreaturesKilled * pointsForKillingLevel1Creature;
-    const pointsForLevel2CreaturesKilled =
-      numOfLevel2CreaturesKilled * pointsForKillingLevel2Creature;
+  const pointsForLevel1CreaturesKilled =
+   numOfLevel1CreaturesKilled * pointsForKillingLevel1Creature;
+  const pointsForLevel2CreaturesKilled =
+   numOfLevel2CreaturesKilled * pointsForKillingLevel2Creature;
   
     // 1000 points for finishing tutorial
-    return tutorialPoints + pointsForLevel1CreaturesKilled + pointsForLevel2CreaturesKilled;
-  }
+  return tutorialPoints + pointsForLevel1CreaturesKilled + pointsForLevel2CreaturesKilled;
+}
